0b5ce76be8808b7d14c1b2c3672a36dca295b85c
Jeff307-kib <wunnatunsai940@gmail.com>
2026-03-01 22:21:26 +0630

    feat/user-create refactor and add update, delte, password hash

--- models/User.js
+++ models/User.js
@@ -1,23 +1,41 @@
+import { query } from "express-validator";
 import pool from "../config/db-config.js";
 
 class User {
   static async create(userData) {
-    const { fullName, email, phoneNumber, passwordHash } = userData;
+    const { fullName, email, phoneNumber, password } = userData;
 
     const query = `
       INSERT INTO users (full_name, email, phone_number, password_hash)
       VALUES ($1, $2, $3, $4)
-      RETURNING user_id, full_name, email, phone_number, created_at;
+      RETURNING user_id, full_name, email, phone_number, created_at, updated_at;
     `;
 
-    const values = [fullName, email, phoneNumber, passwordHash];
+    const values = [fullName, email, phoneNumber, password];
     const result = await pool.query(query, values);
     return result.rows[0];
   }
 
-  static async findById(id) {
+  static async findAll({ limit, sortBy, offset }) {
     const query = `
       SELECT user_id, full_name, email, phone_number, created_at
+      FROM users 
+      ORDER BY ${sortBy ? sortBy : 'created_at'} DESC
+      LIMIT $1 OFFSET $2
+    `;
+
+    const values = [
+      limit,
+      offset
+    ];
+
+    const result = await pool.query(query, values);
+    return result.rows;
+  }
+
+  static async findById(id) {
+    const query = `
+      SELECT user_id, full_name, email, phone_number, created_at, updated_at
       FROM users 
       WHERE user_id = $1;
     `;
@@ -41,6 +59,48 @@ class User {
     `;
 
     const result = await pool.query(query, [phoneNumber]);
+    return result.rows[0];
+  }
+
+  static async updateUser(id, updateData) {
+
+    const allowedFields = [
+      "full_name",
+      "email",
+      "phone_number"
+    ];
+
+    const fields = [];
+    const values = [];
+
+    for (const key of allowedFields) {
+      if (updateData[key] !== undefined) {
+        fields.push(`${key} = $${values.length + 1}`);
+        values.push(updateData[key]);
+      }
+    }
+
+    fields.push("updated_at = NOW()")
+
+    if (fields.length === 1) return null;
+
+    values.push(id);
+
+    const sql = `
+      UPDATE users
+      SET ${fields.join(", ")}
+      WHERE user_id = $${values.length}
+      RETURNING user_id, full_name, email, phone_number, created_at, updated_at;
+    `;
+
+    const result = await pool.query(sql, values);
+    console.log('HELLO FROM USER MODEL');
+    return result.rows[0];
+  }
+
+  static async deleteUser(id) {
+    const query = 'DELETE FROM users WHERE user_id = $1';
+    const result = await pool.query(query, [id]);
     return result.rows[0];
   }
 }
