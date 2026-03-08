4fca5781f47d5c4dc5487ec312857ff90d1d84f3
MoriartyLink <moriartylink@gmail.com>
2026-03-09 02:00:11 +0630

    to extract user_id from req.use

--- controllers/emergency-contact-controller.js
+++ controllers/emergency-contact-controller.js
@@ -9,6 +9,12 @@ export class EmergencyContactController {
         ApiResponse.created(res, contact, 'Contact added successfully');
     });
 
+    getMyContacts = catchAsync(async (req, res) => {
+        const userId = req.user.user_id; 
+        const contacts = await emergencyContactService.getContactsByUserId(userId);
+        ApiResponse.success(res, contacts, 'Your emergency contacts retrieved successfully');
+    });
+
     getById = catchAsync(async (req, res) => {
         const contact = await emergencyContactService.getContactById(req.params.id);
         ApiResponse.success(res, contact, 'Contact retrieved successfully');
